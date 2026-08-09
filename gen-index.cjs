a8e69b3f8481c395f8286263d399ab6d5f22c2c2
SOLO Coder <solo-coder@family-menu.local>
2026-08-09 12:12:21 +0800

    [WP-09/10] 修复process is not defined(添加process polyfill)

--- gen-index.cjs
+++ gen-index.cjs
@@ -8,7 +8,7 @@ const css = fs.readdirSync(path.join(dist, 'css')).filter(f => f.endsWith('.css'
 const cssLinks = css.map(f => '<link rel="stylesheet" href="css/' + f + '">').join('');
 const jsScripts = js.map(f => '<script src="js/' + f + '"></script>').join('');
 
-const html = '<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover"><title>家庭菜谱</title>' + cssLinks + '</head><body><div id="app"></div>' + jsScripts + '</body></html>';
+const html = '<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover"><title>家庭菜谱</title>' + cssLinks + '<script>window.process=window.process||{env:{NODE_ENV:"production"}};</script></head><body><div id="app"></div>' + jsScripts + '</body></html>';
 
 fs.writeFileSync(path.join(dist, 'index.html'), html);
 console.log('index.html created');
