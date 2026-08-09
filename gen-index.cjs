bbdeac3c794e5edb37fb0849e06e4d8000c1bc2b
SOLO Coder <solo-coder@family-menu.local>
2026-08-09 11:57:15 +0800

    [WP-09/10] H5构建后自动生成index.html(Taro4 beta缺少HtmlWebpackPlugin)

---
+++ gen-index.cjs
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+
+const dist = 'apps/h5/dist';
+const js = fs.readdirSync(path.join(dist, 'js')).filter(f => f.endsWith('.js'));
+const css = fs.readdirSync(path.join(dist, 'css')).filter(f => f.endsWith('.css'));
+
+const cssLinks = css.map(f => '<link rel="stylesheet" href="css/' + f + '">').join('');
+const jsScripts = js.map(f => '<script src="js/' + f + '"></script>').join('');
+
+const html = '<!DOCTYPE html><html><head><meta charset="utf-8"><meta name="viewport" content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,viewport-fit=cover"><title>家庭菜谱</title>' + cssLinks + '</head><body><div id="app"></div>' + jsScripts + '</body></html>';
+
+fs.writeFileSync(path.join(dist, 'index.html'), html);
+console.log('index.html created');
+console.log('JS:', js);
+console.log('CSS:', css);
